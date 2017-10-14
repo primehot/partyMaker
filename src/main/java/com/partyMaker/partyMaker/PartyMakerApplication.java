e12689b7ec1ea500cac13ac46fa29a5c5212b8bb
oleh.tsyupa@gmail.com <prime352293>
2017-10-14 19:01:39 +0200

    Create new project

---
+++ src/main/java/com/partyMaker/partyMaker/PartyMakerApplication.java
@@ -0,0 +1,12 @@
+package com.partyMaker.partyMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyMakerApplication.class, args);
+	}
+}
