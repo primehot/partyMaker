a15682ee14d73f8ca017dc97fd3678c5bab1b396
WrocDroid <wrocdroid@gmail.com>
2017-10-14 23:36:01 +0200

    Changing PartyRestController

--- src/main/java/com/partyMaker/partyMaker/rest/PartyRestController.java
+++ src/main/java/com/partyMaker/partyMaker/rest/PartyRestController.java
@@ -1,6 +1,8 @@
 package com.partyMaker.partyMaker.rest;
 
+import com.google.gson.Gson;
 import com.partyMaker.partyMaker.dto.PartyDTO;
+import com.partyMaker.partyMaker.repository.PartyRepository;
 import com.partyMaker.partyMaker.service.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,15 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/parties")
+@RestController
 public class PartyRestController {
 
     @Autowired
     private PartyService partyService;
 
-    @RequestMapping(method = RequestMethod.POST)
+    @Autowired
+    private PartyRepository partyRepository;
+
+    @RequestMapping(value = "/createParty", method = RequestMethod.POST)
     private ResponseEntity createParty(@RequestBody PartyDTO partyInfo) {
         return new ResponseEntity<>("Party Id : " + partyService.createParty(partyInfo) + " was successfully created", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/parties")
+    private ResponseEntity getParties(@RequestBody PartyDTO partyInfo) {
+        return new ResponseEntity<>(new Gson().toJson(partyRepository.findAll()),HttpStatus.OK);
     }
 
 }
