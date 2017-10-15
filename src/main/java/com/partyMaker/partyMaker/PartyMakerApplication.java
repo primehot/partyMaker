2127d6e9e1cc639e4647e85703a6e8070c56dce7
WrocDroid <wrocdroid@gmail.com>
2017-10-15 12:13:01 +0200

    a bit of refactoring and bug fixing

--- src/main/java/com/partyMaker/partyMaker/PartyMakerApplication.java
+++ src/main/java/com/partyMaker/partyMaker/PartyMakerApplication.java
@@ -8,8 +8,8 @@ import com.partyMaker.partyMaker.model.types.CompanyType;
 import com.partyMaker.partyMaker.model.types.PartyType;
 import com.partyMaker.partyMaker.model.types.UserType;
 import com.partyMaker.partyMaker.repository.CompanyRepository;
-import com.partyMaker.partyMaker.repository.PartyRepository;
 import com.partyMaker.partyMaker.repository.LocationRepository;
+import com.partyMaker.partyMaker.repository.PartyRepository;
 import com.partyMaker.partyMaker.repository.UserRepository;
 import com.partyMaker.partyMaker.service.PartyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +26,12 @@ import java.util.List;
 @SpringBootApplication
 public class PartyMakerApplication {
 
-	public static void main(String[] args) {
+    private UserRepository userRepository;
+    private CompanyRepository companyRepository;
+    private PartyRepository partyRepository;
+    private LocationRepository locationRepository;
+
+    public static void main(String[] args) {
 		SpringApplication.run(PartyMakerApplication.class, args);
 	}
 
@@ -40,15 +45,20 @@ public class PartyMakerApplication {
                                   LocationRepository locationRepository) {
 
 		return (evt) -> {
-			generateUsers(userRepository);
-			generateOrganizers(userRepository);
-			generateCompanies(companyRepository);
-			generateParties(partyRepository, userRepository);
-			generateLocation(locationRepository);
+            this.userRepository = userRepository;
+            this.companyRepository = companyRepository;
+            this.partyRepository = partyRepository;
+            this.locationRepository = locationRepository;
+
+            generateUsers();
+			generateOrganizers();
+            generateCompanies();
+            generateParties();
+            generateLocation();
 		};
 	}
 
-	private void generateParties(PartyRepository partyRepository, UserRepository userRepository) {
+	private void generateParties() {
 		Arrays.asList("Jan, Ben, Ola, Agnieszka".split(",")).forEach(
 				name -> {
 					PartyEntity p = new PartyEntity();
@@ -56,24 +66,24 @@ public class PartyMakerApplication {
 					p.setHashtagBox("#homeparty");
 					p.setDate(Date.from(Instant.now()));
 					p.setDescription("Beer and Boobs home party");
-					LocationEntity location = new LocationEntity();
-					location.setAddress("Rynek 1, Wroclaw");
-					location.setAttendersLimit(100);
-					p.setLocation(location);
-					p.setUsers(generateUsers(userRepository));
-					p.setOrganizer(generateOrganizers(userRepository).get(0));
+                    p.setUsers(generateUsers());
+                    p.setOrganizer(generateOrganizers().get(0));
+					p.setLocation(generateLocation().get(0));
+
 					PartyEntity partyEntity = partyRepository.save(p);
-					p.setTickets(partyService.generateTickets(location.getAttendersLimit(),25.0,partyEntity));
+
+					p.setTickets(partyService.generateTickets(partyEntity.getLocation().getAttendersLimit(),25.0,partyEntity));
 				}
 		);
 	}
 
-	private List<UserEntity> generateUsers(UserRepository userRepository) {
+	private List<UserEntity> generateUsers() {
 		Arrays.asList("Jan, Ben, Ola, Agnieszka".split(",")).forEach(
 				name -> {
 					UserEntity u = new UserEntity();
 					u.setName(name);
 					u.setUserType(UserType.CUSTOMER);
+
 					userRepository.save(u);
 				}
 		);
@@ -81,24 +91,26 @@ public class PartyMakerApplication {
 	}
 
 
-	private List<UserEntity> generateOrganizers(UserRepository userRepository) {
+	private List<UserEntity> generateOrganizers() {
 		Arrays.asList("Frank, Andry".split(",")).forEach(
 				name -> {
 					UserEntity u = new UserEntity();
 					u.setName(name);
 					u.setUserType(UserType.ORGANIZER);
+
 					userRepository.save(u);
 				}
 		);
 		return userRepository.findAll();
 	}
 
-	private void generateCompanies(CompanyRepository companyRepository) {
+	private void generateCompanies() {
 		Arrays.asList("House Rent, Castle Rent".split(",")).forEach(
 				name -> {
 					CompanyEntity c = new CompanyEntity();
 					c.setName(name);
 					c.setCompanyType(CompanyType.LOCATION);
+
 					companyRepository.save(c);
 				}
 		);
@@ -107,22 +119,27 @@ public class PartyMakerApplication {
 					CompanyEntity c = new CompanyEntity();
 					c.setName(name);
 					c.setCompanyType(CompanyType.ADDITIONAL);
+
 					companyRepository.save(c);
 				}
 		);
 	}
 
-	private void generateLocation(LocationRepository locationRepository) {
+	private List<LocationEntity> generateLocation() {
 		LocationEntity szewska = new LocationEntity();
 		szewska.setAttendersLimit(200);
 		szewska.setGeoLocation("51.1109220 17.0350210");
 		szewska.setAddress("ul. Szewska 1, Wroclaw");
+
 		locationRepository.save(szewska);
 
 		LocationEntity kuznicza = new LocationEntity();
 		kuznicza.setAttendersLimit(100);
 		kuznicza.setGeoLocation("51.1118580 17.0341073");
 		kuznicza.setAddress("ul. Kuznicza 1, Wroclaw");
+
 		locationRepository.save(kuznicza);
+
+		return locationRepository.findAll();
 	}
 }
